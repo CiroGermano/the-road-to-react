10175ffe63d86e3b52a012d4ef4bc7db92c7ada5
ciromoraes <ciromrs@gmail.com>
2021-06-14 15:29:33 -0300

    lifting-state-in-react

--- hacker-stories/src/App.js
+++ hacker-stories/src/App.js
@@ -20,9 +20,15 @@ const App = () => {
     },
   ];
 
+  const [searchTerm, setSearchTerm] = React.useState("");
+
   const handleSearch = (event) => {
-    console.log(event.target.value);
+    setSearchTerm(event.target.value);
   };
+
+  const searchedStories = stories.filter((storie) =>
+    storie.title.toLowerCase().includes(searchTerm)
+  );
 
   return (
     <div>
@@ -32,27 +38,16 @@ const App = () => {
 
       <hr />
 
-      <List list={stories} />
+      <List list={searchedStories} />
     </div>
   );
 };
 
 const Search = (props) => {
-  const [searchTerm, setSearchTerm] = React.useState("");
-
-  const handleChange = (event) => {
-    setSearchTerm(event.target.value);
-    props.onSearch(event);
-  };
-
   return (
     <div>
       <label htmlFor="search">Search: </label>
-      <input id="search" type="text" onChange={handleChange} />
-
-      <p>
-        Searching for <strong>{searchTerm}</strong>.
-      </p>
+      <input id="search" type="text" onChange={props.onSearch} />
     </div>
   );
 };
