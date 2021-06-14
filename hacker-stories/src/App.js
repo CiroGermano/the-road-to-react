46f769346ab90d30926558761fa9f46ed80d043e
ciromoraes <ciromrs@gmail.com>
2021-06-14 20:44:14 -0300

    react-fragments

--- hacker-stories/src/App.js
+++ hacker-stories/src/App.js
@@ -57,10 +57,10 @@ const App = () => {
 
 const Search = ({ searchTerm, onSearch }) => {
   return (
-    <div>
+    <>
       <label htmlFor="search">Search: </label>
       <input id="search" type="text" value={searchTerm} onChange={onSearch} />
-    </div>
+    </>
   );
 };
 
